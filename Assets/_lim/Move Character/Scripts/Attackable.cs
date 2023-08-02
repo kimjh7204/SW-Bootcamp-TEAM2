0b4b1bb12bf080d168038939b455f82ce51a11d8
liminha <inhag1131@gmail.com>
2023-08-02 15:47:00 +0900

    Create Death Animation

--- Assets/_lim/Move Character/Scripts/Attackable.cs
+++ Assets/_lim/Move Character/Scripts/Attackable.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Attackable : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && HPBar.curHp > 0)
         {
             animator.SetTrigger("attack");
         }
     }
-
-    
 }
