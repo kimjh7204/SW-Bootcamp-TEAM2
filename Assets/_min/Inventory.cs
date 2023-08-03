39f909a23b10b3df290ccd189c27e433747d70d1
BOOK-DLJSUO8O2N\kmkim <kmkim550126@naver.com>
2023-08-03 10:12:47 +0900

    8/2 item

---
+++ Assets/_min/Inventory.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+    public static bool inventoryActivated = false;
+
+    //필요한 컴포넌트
+    [SerializeField]
+    private GameObject go_inventoryUI;
+    [SerializeField]
+    private GameObject go_SlotParent;
+
+    private FieldItems[] slots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slots = go_SlotParent.GetComponentsInChildren<FieldItems>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryOpenInventory();
+    }
+
+    private void TryOpenInventory()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryActivated = !inventoryActivated;
+
+            if(inventoryActivated)
+                OpenInventory();
+            else
+                CloseInventory();
+        }
+    }
+
+    private void OpenInventory()
+    {
+        go_inventoryUI.SetActive(true);
+    }
+    private void CloseInventory()
+    {
+        go_inventoryUI.SetActive(false); 
+    }
+
+    public void AcquireItem(Item _item)
+    {
+
+    }
+}
