36077ec680ee1bed5189e8d2426f84e1217080ce
BOOK-DLJSUO8O2N\kmkim <kmkim550126@naver.com>
2023-08-07 15:46:06 +0900

    8/7 inventory complete

--- Assets/_min/Item/Slot.cs
+++ Assets/_min/Item/Slot.cs
@@ -2,23 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-
     public Items item;
     public int itemCount;
     public Image itemImage;
 
-    [SerializeField] private Text text_Count;
+    [SerializeField] private TextMeshProUGUI text_Count;
     [SerializeField] private GameObject go_Countimage;
 
-    // private void SetColor(float _alpha)
+    // private WeaponManager theWeaponManager;
+
+    // void Start()
     // {
-    //     Color color = itemImage.color;
-    //     color.a = _alpha;
-    //     itemImage.color = color;
+    //     theWeaponManager = FindObjectOfType<WeaponManger>();
     // }
+
+    private void SetColor(float _alpha)
+    {
+        Color color = itemImage.color;
+        color.a = _alpha;
+        itemImage.color = color;
+    }
 
     public void Additem(Items _item, int _count = 1)
     {
@@ -30,13 +38,13 @@ public class Slot : MonoBehaviour
         {
             //go_Countimage.SetActive(true);
             text_Count.text = itemCount.ToString();
+            SetColor(1);
         }
         else
         {
-            text_Count.text ="0";
-            //go_Countimage.SetActive(false);
+            text_Count.text =null;
+            SetColor(1);
         }
-        //SetColor(1);
     }
 
     public void SetSlotCount(int _count)
@@ -53,9 +61,78 @@ public class Slot : MonoBehaviour
         item = null;
         itemCount = 0;
         itemImage.sprite = null;
-        //SetColor(0);
+        SetColor(0);
 
         //go_Countimage.SetActive(false);
-        text_Count.text = "0";
+        text_Count.text = null;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            if(item != null)
+            {
+                if(item.itemType == Items.ItemType.Equipment)
+                {
+                    //StartCoroutine(theWeaponManager.ChangeWeaponCoroutine(item.weaponType, item.itemName));
+                }
+                else
+                {
+                    Debug.Log(item.itemName + " 을 사용했습니다");
+                    SetSlotCount(-1);
+                }
+            }
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if(item != null)
+        {
+            DragSlot.instance.dragSlot = this;
+            DragSlot.instance.DragSetImage(itemImage);
+
+            DragSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(item != null)
+        {
+            DragSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DragSlot.instance.SetColor(0);
+        DragSlot.instance.dragSlot = null;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(DragSlot.instance.dragSlot != null)
+        {
+            ChangeSlot();
+        }
+    }
+
+    private void ChangeSlot()
+    {
+        Items _tempItem = item;
+        int _tempItemCount = itemCount;
+
+        Additem(DragSlot.instance.dragSlot.item, DragSlot.instance.dragSlot.itemCount);
+
+        if(_tempItem != null)
+        {
+            DragSlot.instance.dragSlot.Additem(_tempItem, _tempItemCount);
+        }
+        else
+        {
+            DragSlot.instance.dragSlot.ClearSlot();
+        }
     }
 }
