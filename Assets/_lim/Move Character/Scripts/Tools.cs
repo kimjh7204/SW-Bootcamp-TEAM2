3e41cb206898701ded3978b7f1c43b5ce428c555
liminha <inhag1131@gmail.com>
2023-08-08 12:28:34 +0900

    0808Commit

--- Assets/_lim/Move Character/Scripts/Tools.cs
+++ Assets/_lim/Move Character/Scripts/Tools.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,4 +17,8 @@ public class Tools : MonoBehaviour
     public Type type;
     public int value;
 
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * 10 * Time.deltaTime);
+    }
 }
