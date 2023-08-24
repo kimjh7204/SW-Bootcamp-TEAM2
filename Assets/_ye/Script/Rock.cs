c0675adc16ae52dd77b850da084bbcd0de7d3969
liminha <inhag1131@gmail.com>
2023-08-24 12:41:58 +0900

    Rock minning

--- Assets/_ye/Script/Rock.cs
+++ Assets/_ye/Script/Rock.cs
@@ -15,8 +15,8 @@ public class Rock : MonoBehaviour
 
     [SerializeField]
     private GameObject go_rock;  
-    [SerializeField]
-    private GameObject go_debris; 
+    // [SerializeField]
+    // private GameObject go_debris; 
 
     [SerializeField]
     private GameObject go_effect_prefabs; 
@@ -48,7 +48,7 @@ public class Rock : MonoBehaviour
         col.enabled = false;
         Destroy(go_rock);
 
-        go_debris.SetActive(true);
-        Destroy(go_debris, destroyTime);
+        // go_debris.SetActive(true);
+        // DestroyImmediate(go_debris, true);
     }
 }
